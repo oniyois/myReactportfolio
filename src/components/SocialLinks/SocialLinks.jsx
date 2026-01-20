c0fc4cc1c39ffd30d252d42ae077f74da4654929
oniyois <oniyoislamiyat@gmail.com>
2026-01-20 11:25:02 +0100

    updated social links

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,26 +1,26 @@
 import PropTypes from 'prop-types'
 import { Icon } from '@iconify/react'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
 
 const SocialLinks = ({ data }) => {
-  const [activeLink, setActiveLink] = useState(0);
-  const handleIconHover = (index) => {
-    setActiveLink(index);
-  };
+  const [activeLink, setActiveLink] = useState(0)
 
   return (
     <div className="st-social-link">
       {data.map((item, index) => (
-        <Link
-          to={item.link}
-          className={index === activeLink ? 'st-social-btn active' : 'st-social-btn'}
-          onMouseEnter={() => handleIconHover(index)}
+        <a
           key={index}
+          href={item.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={index === activeLink ? 'st-social-btn active' : 'st-social-btn'}
+          onMouseEnter={() => setActiveLink(index)}
         >
-          <span className="st-social-icon"><Icon icon={`fa6-brands:${item.icon}`} /></span>
+          <span className="st-social-icon">
+            <Icon icon={`fa6-brands:${item.icon}`} />
+          </span>
           <span className="st-icon-name">{item.title}</span>
-        </Link>
+        </a>
       ))}
     </div>
   )
@@ -30,4 +30,4 @@ SocialLinks.propTypes = {
   data: PropTypes.array,
 }
 
-export default SocialLinks;
+export default SocialLinks
